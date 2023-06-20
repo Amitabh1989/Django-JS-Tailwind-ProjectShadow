c7b5d7cd6b0513373feae5c3b4246108dcaa044b
Amitabh Suman <amitabhsuman.ss89@gmail.com>
2023-06-20 09:39:56 +0530

    Working on JS load and adding test step to cart

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Django-JS-Tailwind-ProjectShadow
 A custom website to generate custom JSON data. Personal project. More details to follow.
+
+Django + Docker configuration : https://blog.devgenius.io/django-docker-setup-tutorial-4c3e5f8d6def
--- mysite/testcase/static/testcase/js/test_step.js
+++ mysite/testcase/static/testcase/js/test_step.js
@@ -6,82 +6,46 @@
 
 
 
-function getTestStep() {
-    var testSteps;
-    if (localStorage.getItem('cart') === null) {
-        testSteps = {};
-    } else {
-        testSteps = JSON.parse(localStorage.getItem('cart'));
-    };
-    console.log("Test Case dictionary : " + JSON.stringify(testSteps));
-};
+// function getTestStep() {
+//     var testSteps = localStorage.getItem('testSteps') || {};
+//     // if (testSteps === null) {
+//     //     testSteps = {};
+//     // } else {
+//     //     testSteps = JSON.parse(testSteps);
+//     // }
+//     console.log("Test Case dictionary : " + testSteps);
+//     // localStorage.setItem('testSteps', JSON.stringify(cart));
+// };
 
-getTestStep();
-
-
-/* TO STORE A JSON IN CART, below is the code
-var cart = {
-  test: {
-    name: "amitabh"
-  }
-};
-
-localStorage.setItem("cart", JSON.stringify(cart));
-*/
-
-var addToCartButton = document.querySelectorAll('.')
-
+// getTestStep();
 
 $(document).ready(function() {
-    $(document).on('click', '.atc', function() {
-        console.log("The add to cart button is clicked");
+    $(document).on('click', '.submit-step', function() {
+        console.log("The submit-step button is clicked");
         var item_id = this.id.toString();
-        console.log(item_id);
-        
-        if (cart[item_id] != undefined) {
-            console.log("New item quantity is being added");
-            quantity = cart[item_id][0] + 1;
-            var item_price = document.getElementById("discounted-price" + item_id);
-            cart[item_id][0] = quantity;
-            cart[item_id][2] = parseFloat(cart[item_id][2]) + parseFloat(item_price.innerHTML);
+        console.log("Item added is : " + item_id);
 
-        } else {
-            console.log("New item is being added : ", item_id);
-
-            var item_price = document.getElementById("discounted-price" + item_id);
-            quantity = 1;
-            name = document.getElementById("as" + item_id).innerHTML.trim();
-            price = parseFloat(item_price.innerHTML);
-            cart[item_id] = [quantity, name, price];
-        }
-        console.log("Updated cart : ", cart);
-        localStorage.setItem('cart', JSON.stringify(cart));
-        document.getElementById("cart").innerHTML = "Cart(" + Object.keys(cart).length + ")";
-    });
-    
-    $('#cart').on('click', function () {
-        $(this).popover('show');
-    });
-    
-    function DisplayCart(cart) {
-        var cartString = "";
-        cartString += "<h5>Cart Details</h5><br>"
-        // new code
-        let cartObject = JSON.parse(localStorage.getItem('cart'))
-        for(item in cartObject) {
-            cartString += cartObject[item][1] + ". Qty : " + cartObject[item][0] + "<br>";
-            // cart_index += 1;
-        }
-        cartString += "<br>"
-        cartString += "<a href='" + checkoutUrl + "' class='btn btn-warning' id='checkout'>Checkout</a>";
-        console.log("cartString is : " + cartString);
-        document.getElementById("cart").setAttribute('data-content', cartString);
-        
-        $('[data-toggle="popover"]').popover({
-            html: true,
-            content: cartString
+        // Retrieve the form and its values
+        var form = $(this).closest('form');
+        var formValues = {};
+        form.serializeArray().forEach(function(field) {
+            formValues[field.name] = field.value;
         });
-    }
-    DisplayCart(cart);
-    console.log("Updated cart : ", cart);
+
+        // Retrieve the testSteps from localStorage
+        var testSteps = JSON.parse(localStorage.getItem('testSteps')) || {};
+
+        // Check if 'formValues' property exists
+        if (!testSteps.hasOwnProperty('formValues')) {
+            testSteps.formValues = [];
+        }
+
+        // Append the form values to the 'formValues' array
+        testSteps["formValues"].push(formValues);
+
+        // Update the 'testSteps' in localStorage
+        localStorage.setItem('testSteps', JSON.stringify(testSteps));
+
+        console.log("Updated cart : ", testSteps);
+    });
 });
