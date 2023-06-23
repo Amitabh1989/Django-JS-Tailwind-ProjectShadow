936d98288e39b413b7f6657420dbfcee70cf9b77
Amitabh Suman <amitabhsuman.ss89@gmail.com>
2023-06-23 07:36:17 +0530

    Added JS and Django model for TC and submitting steps as TC in DB

--- mysite/testcase/views.py
+++ mysite/testcase/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import View
-from .models import TestStep
+from .models import TestStep, TestCase
+import time
 # Create your views here.
 
 class TestCaseView(View):
     def post(self, request, *args, **kwargs):
         steps = request.POST.getlist('steps')
+        cqid = request.POST.get("cqid")
+        title = "Dummy_TC_"
+        summary = "Testing Model"
+        tcid = TestCase.objects.get_or_create(cqid=cqid, title=title, summary=summary)
+
         for step in steps:
             TestStep.objects.create(step=step)
+        return HttpResponse("Data Submitted successfully!")
