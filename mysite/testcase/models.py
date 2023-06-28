489700efba25fc3d34aae168501818e2d1e67647
Amitabh Suman <amitabhsuman.ss89@gmail.com>
2023-06-28 09:24:43 +0530

    Modified TestCase and TestStep relationship. Working now.

--- mysite/testcase/models.py
+++ mysite/testcase/models.py
@@ -3,16 +3,33 @@ from django.db import models
 # Create your models here.
 
 
+# class TestCase(models.Model):
+#     cqid = models.CharField(max_length=20)
+#     title = models.CharField(max_length=100)
+#     summary = models.TextField(max_length=2000)
+#     # test_steps_list = models.ManyToManyField('TestStep')
+
+#     def __str__(self) -> str:
+#         return self.title
+
+# class TestStep(models.Model):
+#     test_case = models.ForeignKey(TestCase, on_delete=models.CASCADE, related_name='test_steps')
+#     step = models.CharField(max_length=10000)
+
+#     def __str__(self):
+#         return self.step
+
 class TestCase(models.Model):
     cqid = models.CharField(max_length=20)
     title = models.CharField(max_length=100)
     summary = models.TextField(max_length=2000)
+    test_steps_list = models.ManyToManyField('TestStep')
 
     def __str__(self) -> str:
         return self.title
 
 class TestStep(models.Model):
-    test_case = models.ForeignKey(TestCase, on_delete=models.CASCADE, related_name='test_steps')
+    test_cases = models.ManyToManyField(TestCase, related_name='test_steps')
     step = models.CharField(max_length=10000)
 
     def __str__(self):
