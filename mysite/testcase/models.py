0813373e3b2a9a81091c84d0416b8d3208e968dd
Amitabh Suman <amitabhsuman.ss89@gmail.com>
2023-06-30 09:33:04 +0530

    Working on TestStep analytics

--- mysite/testcase/models.py
+++ mysite/testcase/models.py
@@ -24,13 +24,16 @@ class TestCase(models.Model):
     title = models.CharField(max_length=100)
     summary = models.TextField(max_length=2000)
     test_steps_list = models.ManyToManyField('TestStep')
-
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
     def __str__(self) -> str:
         return self.title
 
 class TestStep(models.Model):
-    test_cases = models.ManyToManyField(TestCase, related_name='test_steps')
-    step = models.CharField(max_length=10000)
+    test_cases = models.ManyToManyField(TestCase, related_name='test_case')
+    step = models.CharField(max_length=3000)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.step
--- mysite/testcase/urls.py
+++ mysite/testcase/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import TestCaseView
+from .views import TestCaseView, TestStepDetail
 
 app_name = "testcase"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="testcase/tchome.html"), name="tchome"),
     # path("success/", TemplateView.as_view(template_name="testcase/success.html"), name="tc_success"),
     path("success/", TestCaseView.as_view(), name="success"),
+    path("teststep_detail/<int:pk>/", TestStepDetail.as_view(), name="teststep_detail")
 ]
