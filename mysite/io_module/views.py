613b8842f808e21a4f7d47ab9fd628b1418116c7
Amitabh Suman <amitabhsuman.ss89@gmail.com>
2023-07-06 22:32:24 +0530

    Added Form name. Will be used to invoke module model.save()

--- mysite/io_module/views.py
+++ mysite/io_module/views.py
@@ -9,3 +9,9 @@ class IOModuleView(CreateView):
     model = IOModule
     form_class = IOModuleForm
     template_name = "io_module/io.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['module_name'] = self.model.module_type.field.get_default()
+        return context
+
