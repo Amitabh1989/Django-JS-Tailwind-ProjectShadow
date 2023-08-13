bfaa068cf5373f0922c0c7ff67e3c726ba380f11
Amitabh Suman <amitabhsuman.ss89@gmail.com>
2023-08-13 18:44:50 +0530

    Fixed usecount, model save and update for IO and Config

--- mysite/io_module/views.py
+++ mysite/io_module/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import render
 from rest_framework.decorators import action
-from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.renderers import TemplateHTMLRenderer, BrowsableAPIRenderer, JSONRenderer
 from django.views.generic import CreateView
 from .forms import IOModelForm
 from .models import IOModel
@@ -19,38 +19,54 @@ class IOModelModelViewSet(viewsets.ModelViewSet):
     serializer_class = IOModelSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = [SessionAuthentication]
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer] 
     # renderer_classes = [TemplateHTMLRenderer]
 
     def create(self, request, *args, **kwargs):
         print("This is POST request")
-        print("Request data is : {}".format(self.request.POST))
+        # print("Request data is : {}".format(self.request.POST.dict()))
         print("Request data is : {}".format(self.request.data))
+        print(f"IO create : User Authenticated : {self.request.user.is_authenticated}")
+        print(f"IO create : User : {self.request.user}")
+        user = self.request.user
         # data = dict(self.request.data)
         # data = {key: value[0] for key, value in data.items()}
-        del self.request.data['csrfmiddlewaretoken']
+        _data = self.request.data.dict()
+        print("Request data is : {}".format(_data))
         query = Q()
-        for fields, value in self.request.data.items():
+        for fields, value in _data.items():
+            if fields == "csrfmiddlewaretoken":
+                continue
             query &= Q(**{f'{fields}': value})
         print(f"Query is : {query}")
 
-        obj = self.queryset.filter(query).first()
+        obj = self.queryset.filter(query).filter(user=self.request.user).first()
+        print(f"IO obj is : {obj}")
+
+        _data["user"] = user.id 
 
         if obj:
-            data = {"_use_count": obj._use_count+1}
+            obj._use_count += 1
+            data = {"_use_count": obj._use_count}
             print(f"Obj.pk : {obj.pk}")
             print(f"Use count : {obj._use_count}")
-            self.partial_update(request, pk=obj.pk, data=data)
+            obj.save()
+            # self.partial_update(request, pk=obj.pk, data=data, user=self.request.user)
+            # self.partial_update(request, pk=obj.pk, data=data)
             return Response({"msg": "Obj already present"}, status=status.HTTP_201_CREATED)
         
-        serialilzer = self.serializer_class(data=self.request.data)
-        if serialilzer.is_valid(raise_exception=True):
-            serialilzer.save()
-            return Response({"msg": "Obj created"}, status=status.HTTP_201_CREATED)
-        return Response(serialilzer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serialilzer = self.serializer_class(data=_data)
+        serialilzer.is_valid(raise_exception=True)
+        serialilzer.save()
+        return Response({"msg": "Obj created"}, status=status.HTTP_201_CREATED)
+        # print(f"Serializer errors : {serialilzer.errors}")
+        # return Response(serialilzer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def partial_update(self, request, pk=None, data=None, *args, **kwargs):
         print(f"Request received here: {request}")
-        obj = self.queryset.get(pk=pk)
+        obj = self.queryset.get(pk=pk, user=self.request.user)
+        # obj = self.get_object()
+        print("IO partial update user is : {}".format(obj))
         serializer = self.serializer_class(data=data, instance=obj, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
