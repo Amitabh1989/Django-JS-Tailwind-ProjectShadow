071850deaca08252e04740fddb2e7c13e5d5eb9e
Amitabh Suman <amitabhsuman.ss89@gmail.com>
2023-08-17 08:24:41 +0530

    UseState and function in UseState Example

--- mysite/frontend/my-react-app/src/components/StudentEvtFunction.js
+++ mysite/frontend/my-react-app/src/components/StudentEvtFunction.js
@@ -1,2 +1,37 @@
 import React, {Component} from "react";
 
+// USING SIMPLE FUNCTION
+// function StudentEvtFunction(props) {
+//     function handleClick() {
+//         console.log("Hey man");
+//         document.getElementById("buddyBtn").innerText = props.city;
+//     }
+
+//     return (
+//         <div id="newDivBuddy">
+//             <h1>Hello {props.name}</h1>
+//             <h3>Function Based Event handling</h3>
+//             <button onClick={handleClick} id="buddyBtn">Click me</button>
+//         </div>
+//     );
+// }
+
+// export default StudentEvtFunction;
+
+
+// EXAMPLE USING ARROW FUNCTION
+function StudentEvtFunction(props) {
+    const handleClick = () => {
+        console.log("Clicked function based evt handler");
+        document.getElementById("btnBudy").innerText = props.city;
+    };
+
+    return (
+        <div>
+            <h1>Hello {props.name}</h1>
+            <button onClick={handleClick} id="btnBudy">Click me arrow fun</button>
+        </div>
+    )
+}
+
+export default StudentEvtFunction;
