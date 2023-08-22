b7039daa3ee65999e25e06a70d3eae8a5c6a10b6
Amitabh Suman <amitabhsuman.ss89@gmail.com>
2023-08-22 09:01:38 +0530

    Added code for Update State

---
+++ mysite/frontend/my-react-app/src/components/StudentUpdateDemo.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Marks from "./Marks";
+
+export default class StudentUpdate extends Component {
+    constructor(props) {
+        console.log("Student update class constriuctor called")
+        super(props);
+        this.state = {
+            marks: this.props.marks
+        };
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log("Parent Update class getDerivedStateCalled");
+    }
+
+    clickHandler = () => {
+        console.log("Clicked");
+        this.setState({marks: "10234"})
+    }
+    render() {
+        console.log("Parent Render called")
+        return (
+            <div>
+                {/* <h1>Old Marks {this.state.marks}</h1> */}
+                <Marks mmarks={this.state.marks}/>
+                <button onClick={this.clickHandler}>Change</button>
+            </div>
+        )
+    };
+}
